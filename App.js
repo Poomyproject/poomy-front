ab74a869dbcc8890cbebdc61a3c692967813c6cb
f020202 <111269736+f020202@users.noreply.github.com>
2024-07-22 10:38:47 +0900

    feat : 로그인 연결 및 BottomTabNavigator추가
    로그인 연결확인 및 메인화면에서 BottomTabNavigator추가 (찜, 홈, 마이페이지)

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+// App.js
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Image } from 'react-native'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,37 +12,75 @@ import TermsScreen from './screens/TermsScreen';
 import NamesetScreen from './screens/NamesetScreen';
 import PreferSelectScreen from './screens/PreferSelectScreen';
 import PerferPlaceScreen from './screens/PreferPlaceScreen';
-import MainScreen from './screens/MainScreen'; 
-import FavoritesScreen from './screens/FavoritesScreen'; 
-import MyPageScreen from './screens/MyPageScreen'; 
+import MainScreen from './screens/MainScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import MyPageScreen from './screens/MyPageScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen 
+        name="Favorites" 
+        component={FavoritesScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '찜',
+          tabBarIcon: ({ color, size }) => (
+            <Image 
+              source={require('./assets/ic_todo.png')} // 로컬 이미지 파일 경로
+              style={{ width: size, height: size, tintColor: color }} // 크기와 색상 설정
+            />
+          ),
+        }} 
+        />
+      <Tab.Screen
+        name="Main"
+        component={MainScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '홈',
+          tabBarIcon: ({ color, size }) => (
+            <Image 
+              source={require('./assets/ic_home.png')} // 로컬 이미지 파일 경로
+              style={{ width: size, height: size, tintColor: color }} // 크기와 색상 설정
+            />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="MyPage" 
+        component={MyPageScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '마이페이지',
+          tabBarIcon: ({ color, size }) => (
+            <Image 
+              source={require('./assets/ic_mypage.png')} // 로컬 이미지 파일 경로
+              style={{ width: size, height: size, tintColor: color }} // 크기와 색상 설정
+            />
+          ),
+        }} 
+        />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
         <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Terms" component={TermsScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Nameset" component={NamesetScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="PreferSelect" component={PreferSelectScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="PreferPlace" component={PerferPlaceScreen}options={{ headerShown: false }}/>
+        <Stack.Screen name="Terms" component={TermsScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Nameset" component={NamesetScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="PreferSelect" component={PreferSelectScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="PreferPlace" component={PerferPlaceScreen} options={{ headerShown: false }} />
         <Stack.Screen name="MainTab" component={MainTabNavigator} options={{ headerShown: false }} />
-        <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const MainTabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
-      <Tab.Screen name="Favorites" component={FavoritesScreen} />
-      <Tab.Screen name="MyPage" component={MyPageScreen} />
-    </Tab.Navigator>
-  );
-};
