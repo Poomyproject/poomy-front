809f3fdc43990d89b0cae8d8a5f308d0dc846135
f020202 <111269736+f020202@users.noreply.github.com>
2024-08-03 20:58:47 +0900

    feat: MainScreen.js 더보기 삭제

--- screens/KeywardRecmdScreen.js
+++ screens/KeywardRecmdScreen.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import colors from '../config/colors';
 
 
 const KeywardRecmdScreen = () => {
-  return (
-    <View style={styles.container}>
-      <View horizontal={true} style={styles.placeContainer} showsHorizontalScrollIndicator={false}>
-                        <TouchableOpacity>
-                            <Text style={styles.keywordText}>아기자기한</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text style={styles.keywordText}>모던</Text>
-                        </TouchableOpacity>
-                        </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={{marginTop:'5%'}}></View>
+            <View horizontal={true} style={styles.placeContainer}>
+                <TouchableOpacity>
+                    <Text style={styles.keywordText}>장소</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.keywordText}>모던</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center',
-        backgroundColor:'white'
-        
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'white'
+
     },
     keywordText: {
         alignSelf: 'flex-start',
-        padding: 5, // 텍스트 주위의 여백
+        padding: 10, // 텍스트 주위의 여백
         paddingHorizontal: 12,
         borderWidth: 1, // 테두리 두께
         borderColor: colors.Green500, // 테두리 색상
         color: colors.Gray700,
-        borderRadius: 13, // 테두리 모서리 둥글게 하기
+        borderRadius: 18, // 테두리 모서리 둥글게 하기
         textAlign: 'center', // 텍스트 가운데 정렬 (필요시)
         marginHorizontal: 5,
         fontSize: '14%',
