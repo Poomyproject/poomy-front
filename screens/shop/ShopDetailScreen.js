23230c5dca3fb2772e3a91bb690e0c9c16d065af
Simmee02 <20211002@sungshin.ac.kr>
2024-10-21 16:18:05 +0900

    feat : 디테일 랜더링

--- screens/shop/ShopDetailScreen.js
+++ screens/shop/ShopDetailScreen.js
@@ -1,6 +1,7 @@
 import React , {useContext , useState , useEffect} from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity,ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity,ScrollView, FlatList } from 'react-native';
 import { MaterialIcons, FontAwesome } from '@expo/vector-icons'; // 아이콘 사용
+import Swiper from 'react-native-swiper';
 import colors from '../../config/colors';
 import { fonts } from '../../config/fonts'; 
 import { useNavigation } from '@react-navigation/native';
@@ -21,8 +22,9 @@ const ShopDetailScreen = () => {
     const fetchShopData = async () => {
       try {
         const response = await ApiClient.get(`/api/shop/${selectedShopId}`); // shopId를 이용해 API 호출
-        setShopData(response.data); // API 응답 데이터를 상태에 저장
+        setShopData(response.data.response);  // API 응답 데이터를 상태에 저장
         console.log('API 응답:', response.data); // 응답 데이터 출력
+        console.log('shopImageList 데이터:', response.data.response.shopImageList); // 확인용 로그
       } catch (err) {
         console.error('API 요청 중 에러 발생:', err);
         setError(err);
@@ -37,7 +39,7 @@ const ShopDetailScreen = () => {
   }, [selectedShopId]);
 
   if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
+    return <ActivityIndicator size="large" color={colors.Green900}/>;
   }
 
   if (error) {
@@ -50,41 +52,81 @@ const ShopDetailScreen = () => {
 
 
     return (
-      <ScrollView contentContainerStyle={{ alignItems: 'center', flexGrow: 1 }}>
-            <Image source={require('../../assets/photo.png')}></Image>
-            <View style={styles.header}>
-        <Text style={styles.shopName}>선민이네 샵</Text>
-        <TouchableOpacity>
-          <Image source={require('../../assets/heart.png')} style={styles.icon}/>
-        </TouchableOpacity>
+      <ScrollView contentContainerStyle={{ backgroundColor : colors.Ivory100 , alignItems: 'center', flexGrow: 1 }}>
+        
+      {shopData && shopData.shopImageList && shopData.shopImageList.length > 0 ? (
+      <Swiper 
+      style={styles.swiper} 
+      showsPagination={true} 
+      dotColor= "#D4D4D4" // 기본 점 색상
+      activeDotColor="#666666"  // 활성 점 색상
+      paginationStyle={styles.pagination}  // 점의 위치 조정
+    >
+      {shopData.shopImageList.map((image, index) => (
+      <View key={index} style={styles.slide}>
+        <Image 
+          source={{ uri: image.url }}  // 각 이미지의 URL을 사용
+          style={styles.shopImage}
+        />
       </View>
-      
-      <View style={styles.infoContainer}>
-        <View style={styles.infoRow}>
-            <Image source={require('../../assets/clock.png')} style={styles.icon}/>
-          <Text style={styles.infoText}>09:00 - 15:00</Text>
-        </View>
-        <View style={styles.infoRow}>
-        <Image source={require('../../assets/phone.png')} style={styles.icon}/>
-          <Text style={styles.infoText}>000-0000-0000</Text>
-        </View>
-        <View style={styles.infoRow}>
-        <Image source={require('../../assets/map-pin_gray.png')} style={styles.icon}/>
-          <Text style={styles.infoText}>서울시 용산구 | 이태원역 1번 출구 9분</Text>
-        </View>
-      </View>
-      <Image source = {require('../../assets/img_map.png')} style ={{marginTop: 10}}></Image>
-      <View style={styles.header}>
-      <TouchableOpacity 
-        style={{ flexDirection: 'row', alignItems: 'center' }} 
-        onPress={() => navigation.navigate('UserReview1', { screen: 'UserReview1' })} // UserReviewScreen1로 이동
-        >
-      <Text style={styles.shopName}>리뷰</Text>
-      <Image source={require('../../assets/edit.png')} style={{ marginLeft: 230 }} />
-      <Text style={styles.infoText}>작성하기</Text>
-      </TouchableOpacity>
-        </View>
-    <View style={styles.reviewSection}>
+    ))}
+  </Swiper>
+  ) : (
+  <Image 
+    source={require('../../assets/photo.png')}  // 기본 이미지
+    style={styles.shopImage}
+  />
+)}
+
+              <View style={styles.header}>
+                
+              {shopData ? (
+                <>
+                <Text style={styles.shopName}>{shopData.name || '이름 없음'}</Text>
+                  <TouchableOpacity>
+                    <Image source={require('../../assets/heart.png')} style={styles.icon} />
+                  </TouchableOpacity>
+                </>
+              ) : (
+                <Text>상점 이름 없음</Text>
+              )}
+            </View>
+
+            {shopData && (
+              <>
+                <View style={styles.infoContainer}>
+                  <View style={styles.infoRow}>
+                    <Image source={require('../../assets/clock.png')} style={styles.icon} />
+                    <Text style={styles.infoText}>{shopData.openingHours ? shopData.openingHours : '영업시간 정보 없음'}</Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Image source={require('../../assets/phone.png')} style={styles.icon} />
+                    <Text style={styles.infoText}>{shopData.phoneNumber ? shopData.phoneNumber: '번호 정보 없음' }</Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Image source={require('../../assets/map-pin_gray.png')} style={styles.icon} />
+                    <Text style={styles.infoText}>
+                    {shopData.location ? shopData.location : '위치 정보 없음'}
+                    {'\n'}{shopData.nearbyStation ? shopData.nearbyStation : '인근 지하철 정보 없음'}
+                    </Text>
+                  </View>
+                </View>
+
+                <Image source={require('../../assets/img_map.png')} style={{ marginTop: 10 }} />
+              </>
+            )}
+
+
+<TouchableOpacity
+  style={{ flexDirection: 'row', alignItems: 'center' }}
+  onPress={() => navigation.navigate('UserReview1', { screen: 'UserReview1' })} // UserReviewScreen1로 이동
+>
+  <Text style={styles.shopName}>리뷰</Text>
+  <Image source={require('../../assets/edit.png')} style={{ marginLeft: 230 }} />
+  <Text style={styles.infoText}>작성하기</Text>
+</TouchableOpacity>
+
+<View style={styles.reviewSection}>
   <Text style={styles.subTitle}>13명의 추천을 받은 소품샵이에요</Text>
 
   {/* 이미지 및 리뷰 상단 */}
@@ -171,10 +213,8 @@ const ShopDetailScreen = () => {
     </View>
   </View>
 </View>
-    
 </ScrollView>
-    );
-
+  );
  };
 
  const styles = StyleSheet.create({
@@ -182,6 +222,7 @@ const ShopDetailScreen = () => {
         flexGrow : 1,
         marginTop : 10, 
         alignItems : 'center',
+        backgroundColor : colors.Ivory100
     },
     header: {
         flexDirection: 'row',
@@ -192,6 +233,23 @@ const ShopDetailScreen = () => {
         width: 380,
         marginBottom: 10,
         marginTop : 10,
+      },
+      swiper: {
+        height: 200,  // Swiper의 높이 조정
+      },
+      slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      shopImage: {
+        width: '90%',  // 부모 너비에 맞춤 (match_parent)
+        height: 168,    // 세로는 고정된 168
+        resizeMode: 'cover',  // 이미지가 잘리지 않도록 커버
+        borderRadius: 4,
+      },
+      pagination: {
+        bottom: -5,  // 점을 이미지 아래로 배치
       },
       icon: {
         width: 20, 
@@ -205,7 +263,7 @@ const ShopDetailScreen = () => {
       },
       infoContainer: {
         marginTop: 10,
-        alignItems: 'left',
+        alignItems: 'flex-start',
         marginLeft : -90 , 
       },
       infoRow: {
