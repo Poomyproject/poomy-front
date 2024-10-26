0dc38b157ecd09eb083a2b0d43eddf7f955b2f59
Simmee02 <20211002@sungshin.ac.kr>
2024-10-27 01:09:49 +0900

    feat : 찜기능 추가 , 메인화면 리프레쉬 처리

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './screens/navigation/AppNavigator';
 import Initializer from './Initializer';
 import { Toast, toastConfig } from './screens/search/Toast';
-import { FavoriteProvider } from './screens/like/LikeContext'; 
-import ShopProvider from './screens/shop/ShopContext';
-import SpotProvider from './screens/keyword/KeywordContext';
-import MoodProvider from './screens/keyword/MoodContext'
-import NewsLetterProvider from './screens/context/NewsLetterContext';
+import { FavoriteProvider } from './screens/like/FavoriteContext'; 
+import  ShopProvider from './screens/shop/ShopContext';
+import  SpotProvider  from './screens/keyword/KeywordContext';
+import  MoodProvider from './screens/keyword/MoodContext';
+import  NewsLetterProvider  from './screens/context/NewsLetterContext';
 
 
 export default function App() {
